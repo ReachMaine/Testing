ee0a5a7d799baa21feb124446431448efd50e5e3
Tim Suellentrop <tim@timsuellentrop.com>
2016-08-24 15:40:36 -0400

    Toggle class on image

--- Avada-Child-Theme/custom/ea_expand_image.js
+++ Avada-Child-Theme/custom/ea_expand_image.js
@@ -1,1 +1,8 @@
-function eaExpandImage() { this.style.color = red; }
+jQuery(document).ready(function(){
+
+	function eaExpandImage() { this.style.color = red; };
+
+	jQuery("img").click(function(){
+	jQuery(this).parent().toggleClass('expanded-image');('expanded-image');
+	});
+});	
