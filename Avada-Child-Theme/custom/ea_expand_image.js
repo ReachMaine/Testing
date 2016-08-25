7726ee736f1368c2522a5975032d9b310053c4ce
Linda Speight <linda@reachmaine.com>
2016-08-25 15:22:29 -0400

    add ea-snap-image
    so that safari doesnt flip out.

--- Avada-Child-Theme/custom/ea_expand_image.js
+++ Avada-Child-Theme/custom/ea_expand_image.js
@@ -3,5 +3,10 @@ jQuery(document).ready(function(){
 	jQuery("img.ea-expandable").click(function(){
 	jQuery(this).parent().toggleClass('ea-contracted-image');
 	jQuery(this).parent().toggleClass('ea-expanded-image');
+	isSafari = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/);
+	if (isSafari) {
+		jQuery(this).addClass("ea-snap-image");
+		jQuery(this).parent().addClass("ea-snap-image");
+	}
 	});
 });	
